0a71c6b786bcf71b9094000220d368d6fb02c1d3
randerous <1521498542@qq.com>
2023-04-04 21:27:43 +0800

    create attack graph

--- AttackPathGenerator/src/adjustmentWork/adjustment.java
+++
@@ -1,5 +0,0 @@
-package adjustmentWork;
-
-public class adjustment {
-
-}
---
+++ AttackPathGenerator/src/attackGraph/attackVertex.java
@@ -0,0 +1,15 @@
+package attackGraph;
+
+public class vertex {
+    public int type;// surface 1, asset 2;
+    public String name;
+    public String archType;
+
+    public vertex(int t, String name, String archType)
+    {
+        this.type = t;
+        this.name = name;
+        this.archType = archType;
+    }
+
+}
